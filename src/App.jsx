14c27068f8efde68f396c288f423491512d88bf9
mreza-slt <rsltany649@gmail.com>
2025-02-02 14:09:20 +0330

    Create Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-      <h1 style={{color:"white"}}>Rick and Morty Api</h1>
+      <Navbar />
     </>
   );
 }
