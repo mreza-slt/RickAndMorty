94cc7554d68f667ae282fb477552acb5375413c1
mreza-slt <rsltany649@gmail.com>
2025-02-02 17:00:28 +0330

    Add characterList Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { allCharacters } from "../data/data";
 import "./App.css";
+import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Navbar />
-    </>
+      <div className="main">
+        <CharacterList characters={allCharacters} />
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,33 @@
+import { EyeIcon } from "@heroicons/react/24/outline";
+
+export default function CharacterList({ characters }) {
+  return (
+    <div className="characters-list">
+      {characters.map((item) => (
+        <Character key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+function Character({ item }) {
+  return (
+    <div className="list__item">
+      <img src={item.image} alt={item.name} />
+      <h3 className="name">
+        <span style={{color:"red"}}>{item.gender === "Male" ? "male" : "female"}</span>
+        <span>{item.name}</span>
+      </h3>
+      <div className="list-item__info info">
+        <span
+          className={`status ${item.status === "Dead" ? "red" : ""}`}
+        ></span>
+        <span> {item.status} </span>
+        <span> - {item.species}</span>
+      </div>
+      <button className="icon red">
+        <EyeIcon />
+      </button>
+    </div>
+  );
+}
