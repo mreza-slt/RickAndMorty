6e01d9e3213c0a2d0833a173309b5724bff2bf20
mreza-slt <rsltany649@gmail.com>
2025-02-08 21:44:24 +0330

    add loading component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "./App.css";
 import CharacterDetail from "./components/CharacterDetail";
 import CharacterList from "./components/CharacterList";
 import Navbar, { SearchResult } from "./components/Navbar";
+import Loading from "./components/Loading";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // useEffect(() => {
   //   fetch("https://rickandmortyapi.com/api/character")
@@ -15,24 +17,32 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const res = await fetch("https://rickandmortyapi.com/api/character");
       const data = await res.json();
       setCharacters(data.results.slice(0, 3));
+      setIsLoading(false);
     }
 
     fetchData();
   }, []);
 
   return (
-    <div className="app">
-      <Navbar>
-        <SearchResult numOfResult={characters.length} />
-      </Navbar>
-      <Main>
-        <CharacterList characters={characters} />
-        <CharacterDetail />
-      </Main>
-    </div>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="app">
+          <Navbar>
+            <SearchResult numOfResult={characters.length} />
+          </Navbar>
+          <Main>
+            <CharacterList characters={characters} />
+            <CharacterDetail />
+          </Main>
+        </div>
+      )}
+    </>
   );
 }
 
